e68da1e8f196403255496f24f903b77f0315ed55
Nicolay Kosmachev <KolyaKos@tut.by>
2013-11-14 17:47:24 +0300

    fgh

--- PhoneGapBuild/SpaPhoneGap/Scripts/phoneGapApp/app.viewmodel.js
+++ PhoneGapBuild/SpaPhoneGap/Scripts/phoneGapApp/app.viewmodel.js
@@ -173,11 +173,9 @@
     self.onDeviceReady = function () {
         
         self.initialize();
-        var ref = window.open('http://www.google.com');
+        var ref = window.open('http://www.google.com', '_blank', 'location=yes');
         
-        ref.addEventListener('loadstart', function () { self.localPage(event.url); });
-
-        ref.close();
+        ref.addEventListener('loadstart', function () { alert(event.url); });
     };
 
     self.initialize = function() {
