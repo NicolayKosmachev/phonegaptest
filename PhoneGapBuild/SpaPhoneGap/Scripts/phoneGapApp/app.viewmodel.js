c70e0a39a32fbf3e870e0bce6a95d9ace91f0622
Nicolay Kosmachev <KolyaKos@tut.by>
2013-11-15 17:05:43 +0300

    cdfadc

--- PhoneGapBuild/SpaPhoneGap/Scripts/phoneGapApp/app.viewmodel.js
+++ PhoneGapBuild/SpaPhoneGap/Scripts/phoneGapApp/app.viewmodel.js
@@ -177,6 +177,7 @@
     self.onDeviceReady = function () {
         alert("test");
         var ref = window.open('http://apache.org', '_blank');
+        ref.show();
         //ref.addEventListener('loadstart', function () { alert('start: ' + event.url); });
         //ref.addEventListener('loadstop', function () { alert('stop: ' + event.url); });
         //ref.addEventListener('exit', function () { alert(event.type); });
